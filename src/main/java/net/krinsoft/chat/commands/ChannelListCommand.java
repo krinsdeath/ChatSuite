0020a280c7284c7572d7f1885bb78d317788f318
Jeff Wardian <krinsdeath@hotmail.com>
2011-08-23 18:29:05 -0700

    forgot to add this file;

---
+++ src/main/java/net/krinsoft/chat/commands/ChannelListCommand.java
@@ -0,0 +1,46 @@
+package net.krinsoft.chat.commands;
+
+import java.util.List;
+import net.krinsoft.chat.ChatCore;
+import net.krinsoft.chat.targets.ChatPlayer;
+import net.krinsoft.chat.util.ColoredMessage;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionDefault;
+
+/**
+ *
+ * @author krinsdeath
+ */
+public class ChannelListCommand extends ChatSuiteCommand {
+
+    public ChannelListCommand(ChatCore plugin) {
+        super(plugin);
+        this.plugin = (ChatCore) plugin;
+        this.setName("chatsuite list");
+        this.setCommandUsage("/cs help list");
+        this.setArgRange(0, 0);
+        this.addKey("cs list");
+        this.addKey("cslist");
+        this.addKey("csl");
+        this.setPermission("chatsuite.list", "Allows users to list the channels they're in.", PermissionDefault.TRUE);
+    }
+
+    @Override
+    public void runCommand(CommandSender sender, List<String> args) {
+        if (!(sender instanceof Player)) {
+            return;
+        }
+        ChatPlayer p = plugin.getPlayerManager().getPlayer((Player) sender);
+        List<String> channels = plugin.getChannelManager().listChannels(p.getName());
+        int i = 0;
+        ColoredMessage msg = new ColoredMessage(plugin.getLocaleManager().getMessage(p.getLocale(), "list_channels"));
+        for (String chan : channels) {
+            for (String line : msg.getContents()) {
+                sender.sendMessage(line.replaceAll("%c", chan).replaceAll("%num", ""+i));
+            }
+            i++;
+        }
+    }
+
+}
